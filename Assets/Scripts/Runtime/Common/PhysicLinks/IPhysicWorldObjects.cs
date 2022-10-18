c29062333b6f06e8d676dd5fd0f9d2dad52321fa
dpankevich <dpankevich@saber.games>
2022-10-18 19:16:32 +0300

    Cleanup unused, move files

--- Assets/Scripts/Runtime/Common/PhysicLinks/IPhysicWorldObjects.cs
+++
@@ -1,8 +0,0 @@
-namespace GameLibrary
-{
-    public interface IPhysicWorldObjects<TLinkedObject> : IReadOnlyPhysicWorldObjects<TLinkedObject>
-    {
-        void Link(IPhysicalObject physicalObject, TLinkedObject linkedObject);
-        void Unlink(IPhysicalObject physicalObject);
-    }
-}
--- Assets/Scripts/Runtime/Common/PhysicLinks/IReadOnlyPhysicWorldObjects.cs
+++
@@ -1,8 +0,0 @@
-namespace GameLibrary
-{
-    public interface IReadOnlyPhysicWorldObjects<TLinkedObject>
-    {
-        bool HasLinkedObject(IPhysicalObject physicalObject);
-        TLinkedObject GetLinkedObject(IPhysicalObject physicalObject);
-    }
-}
