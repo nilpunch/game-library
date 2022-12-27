acc7e2a1132cab6ed2846d9ce21e39234ba641fd
Daniil Pankevich <daniil.pankevich@gmail.com>
2022-12-27 22:21:51 +0300

    Update physics

--- Assets/Scripts/Testing/SMF/SmfCollisionTest.cs
+++ Assets/Scripts/Testing/SMF/SmfCollisionTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using GameLibrary.Mathematics;
+using GameLibrary.Physics;
 using GameLibrary.Physics.SupportMapping;
 using GameLibrary.Physics.SupportPoint;
 using UnityEditor;
@@ -28,6 +29,45 @@ namespace SMF
         [SerializeField, Range(1, 100)] private int _gjkIterations = 20;
         [SerializeField, Range(1, 100)] private int _epaIterations = 20;
 
+        private void Update()
+        {
+            var sphere = new SphereCollider(_firstSphere.position.ToSoft(), (SoftFloat)_firstRadius);
+            var sphere2 = new SphereCollider(_secondSphere.position.ToSoft(), (SoftFloat)_secondRadius);
+            var box = new BoxCollider(_boxOrigin.position.ToSoft(), (_boxExtents / 2f).ToSoft());
+            var box2 = new BoxCollider(_boxOrigin2.position.ToSoft(), (_boxExtents2 / 2f).ToSoft());
+
+            var shapeA = sphere;
+            var shapeB = sphere2;
+
+            GjkAlgorithm.Result result = GjkAlgorithm.Calculate(shapeA, shapeB, _gjkIterations);
+
+            // foreach (var edge in result.Simplex.DistinctPairs((a, b) => (a, b)))
+            // {
+            //     Debug.DrawLine(edge.a.ToUnity(), edge.b.ToUnity(), Color.yellow);
+            // }
+            //
+            // Debug.DrawLine(Vector3.zero, result.Direction.ToUnity(), Color.blue);
+
+
+            if (result.CollisionHappened)
+            {
+                var epaResult = EpaAlgorithm.Calculate(result.Simplex, shapeA, shapeB, _epaIterations);
+
+                foreach (var face in epaResult.polytopeFaces)
+                {
+                    Debug.DrawLine(epaResult.polytope[face.A].ToUnity(), epaResult.polytope[face.B].ToUnity(), Color.magenta);
+                    Debug.DrawLine(epaResult.polytope[face.A].ToUnity(), epaResult.polytope[face.C].ToUnity(), Color.magenta);
+                    Debug.DrawLine(epaResult.polytope[face.B].ToUnity(), epaResult.polytope[face.C].ToUnity(), Color.magenta);
+                }
+
+
+
+                Debug.DrawLine(epaResult.collision.Contacts[0].Position.ToUnity(),
+                    (epaResult.collision.Contacts[0].Position + epaResult.collision.PenetrationNormal * epaResult.collision.PenetrationDepth).ToUnity(),
+                    Color.cyan);
+            }
+        }
+
         private void OnDrawGizmos()
         {
             if (_firstSphere == null || _secondSphere == null || _boxOrigin == null)
@@ -39,42 +79,16 @@ namespace SMF
             var box2 = new BoxCollider(_boxOrigin2.position.ToSoft(), (_boxExtents2 / 2f).ToSoft());
 
             var shapeA = sphere;
-            var shapeB = box;
+            var shapeB = sphere2;
 
             GjkAlgorithm.Result result = GjkAlgorithm.Calculate(shapeA, shapeB, _gjkIterations);
 
-            // Gizmos.color = Color.magenta;
-            // foreach (var (a, b) in result.Simplex.DistinctPairs((a, b) => (a, b)))
-            // {
-            //     Gizmos.DrawLine(a.ToUnity(), b.ToUnity());
-            // }
-
             if (result.CollisionHappened)
             {
-                var epaResult = EpaAlgorithm.Calculate(result.Simplex, shapeA, shapeB, _epaIterations);
-
-                //Gizmos.color = Color.magenta;
-                // foreach (var face in epaResult.polytopeFaces)
-                // {
-                //     Gizmos.DrawLine(epaResult.polytope[face.A].ToUnity(), epaResult.polytope[face.B].ToUnity());
-                //     Gizmos.DrawLine(epaResult.polytope[face.A].ToUnity(), epaResult.polytope[face.C].ToUnity());
-                //     Gizmos.DrawLine(epaResult.polytope[face.B].ToUnity(), epaResult.polytope[face.C].ToUnity());
-                // }
-
-                Gizmos.color = Color.cyan;
-
-                Gizmos.DrawLine(epaResult.Contacts[0].Position.ToUnity(), (epaResult.Contacts[0].Position + epaResult.PenetrationNormal * epaResult.PenetrationDepth).ToUnity());
-
                 Gizmos.color = Color.green;
             }
             else
             {
-                Gizmos.color = Color.yellow;
-                // foreach (var (a, b) in result.Simplex.DistinctPairs((a, b) => (a, b)))
-                // {
-                //     Gizmos.DrawLine(a.ToUnity(), b.ToUnity());
-                // }
-
                 Gizmos.color = Color.red;
             }
 
@@ -82,16 +96,16 @@ namespace SMF
             // Debug.Log(result.Iterations + ": " + result.Direction);
 
             Gizmos.DrawWireSphere(Vector3.Scale(_firstSphere.position, new Vector3(1f, 1f, 1f)), _firstRadius);
-            //
-            // Gizmos.DrawWireSphere(Vector3.Scale(_secondSphere.position, new Vector3(1f, 1f, 1f)), _secondRadius);
+
+            Gizmos.DrawWireSphere(Vector3.Scale(_secondSphere.position, new Vector3(1f, 1f, 1f)), _secondRadius);
 
             // if (GJK2D.IsColliding(box, sphere) || GJK2D.IsColliding(box, sphere2))
             //     Gizmos.color = Color.green;
             // else
             //     Gizmos.color = Color.red;
 
-            Gizmos.DrawWireCube(Vector3.Scale(_boxOrigin.position, new Vector3(1f, 1f, 1f)),
-                Vector3.Scale(_boxExtents, new Vector3(1f, 1f, 1f)));
+            // Gizmos.DrawWireCube(Vector3.Scale(_boxOrigin.position, new Vector3(1f, 1f, 1f)),
+            //     Vector3.Scale(_boxExtents, new Vector3(1f, 1f, 1f)));
             // Gizmos.DrawWireCube(Vector3.Scale(_boxOrigin2.position, new Vector3(1f, 1f, 1f)),
             //     Vector3.Scale(_boxExtents2, new Vector3(1f, 1f, 1f)));
         }
